44a742920c2fd3dc210825d186ae8d08b00cdeca
Kenny Herrera <kenny.herrera@hotmail.com>
2026-08-30 07:38:10 +0200

    add search_agent module

---
+++ search_agent.py
@@ -0,0 +1,27 @@
+from re import S
+from agents.model_settings import ModelSettings
+from agents import Agent, WebSearchTool
+from config import MODEL_NAME
+
+
+INSTRUCTIONS = """
+You are a research assistant. Given a search term, you search the web for that term and 
+produce a concise summary of the results. The summary must 2-3 paragraphs and less than 300 words.
+Capture the main points and be succinct. Reply only with the summary.
+"""
+task = "Most popular AI Agent frameworks in 2026"
+
+settings = ModelSettings(tool_choice="required")
+tools = [WebSearchTool()]
+
+search_agent = Agent(name="search_agent", 
+                            tools=tools, 
+                            settings=settings,
+                            instructions=INSTRUCTIONS,
+                            model=MODEL_NAME)
+
+
+
+
+
+
